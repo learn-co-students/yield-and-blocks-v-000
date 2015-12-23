922f390db0b20f362114592e92fa738ea2959d1f
jj54321 <mailme711@gmail.com>
2015-12-23 23:45:40 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,15 @@
 def hello_t(array)
   i = 0
   while i < array.length
-    yield array[i]
-        i = i + 1
+    yield(array[i])
+    i += 1
   end
   array
 end
 
 # call your method here!
-test = ["Tim", "Tom", "Jim"]
 
-hello_t(test) do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
     if name.start_with?("T")
         puts "Hi, #{name}"
     end
