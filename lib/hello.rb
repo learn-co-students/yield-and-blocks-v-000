600929c19003d0028e3e81ccb93bdc9906074705
Chris Aporta <christopher.aporta@gmail.com>
2015-12-30 21:42:26 -0500

    improve lab

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,14 @@
+#def hello_t(arr)
+  #arr.each do |el|
+    #puts "Hi, #{el}" if el[0] == 'T'
+  #end
+#end
+
 def hello_t(arr)
-  arr.each do |el|
-    puts "Hi, #{el}" if el[0] == 'T'
+  i = 0
+  while i < arr.length
+    yield arr[i]
+    i += 1
   end
+  arr
 end
