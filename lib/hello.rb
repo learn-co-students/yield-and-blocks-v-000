a370ad4fba7473eb0cfdf270c27edab2d2ced400
CodeByLine <yleventhal@ymail.com>
2016-02-16 19:37:49 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
+def hello_t(arraym)
+  i = 0
+  while i < arraym.length
+    yield array[i]
+    i = i + 1
+  end
 
 end
 
-# call your method here!
+def hello_t(arraym)
+  is = 0
+  while is < arraym.length
+    yield(arraym[is])
+      is = is + 1
+  end
+  arraym
+end
 
+# call your method here!
+#hello_t(arraym)
