eb03c79a9e0de75a3f332547f7a907042b4bcedb
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-05-01 17:57:34 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+# step 1: define method to accept an argument 
+def hello_t(array)
+# step 2: ENACTING AN INTERACTION. using WHILE loop to create our iteration
+  i = 0
+  while i < array.length #tell while loop to execute the code in between while & end as long as 'i' is less than
+    #lenght of the array 
 
+#step3: yeilding to the block
+  yield array[i] #<--[] used 2 grab value of each succussive index element as we proceed through our while loop & yield to a block
+      i = i + 1
+  #step 4: call our method 
+ end 
+ array    # <-----
 end
 
-# call your method here!
+
 
