670ca14ec1a44fc6233d817d047282938a044e35
Keegan Leitz <kjleitz@gmail.com>
2016-10-31 15:51:37 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
-
+def hello_t(array)
+  if !block_given?
+    puts "Hey! No block was given!"
+    return
+  end
+  array.length.times do |i|
+    yield array[i]
+  end
+  array
 end
 
 # call your method here!
+hello_t([1, 2, 3]) do |i|
+  puts "number #{i}"
+end
 
+hello_t([1, 2, 3])
