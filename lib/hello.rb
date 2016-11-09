f20420aab280c57df1d14f08b508aa05f7e1959d
David Lewitan <dovidlwtn@gmail.com>
2016-11-09 02:52:01 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,15 @@
-def hello_t
-
+def hello_t(arr)
+  index = 0
+  while index < arr.count
+    yield arr[index]
+    index += 1
+  end
+  arr
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name[0] == 'T'
+    puts "Hi, #{name}"
+  end
+end
