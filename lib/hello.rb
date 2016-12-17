e664cf91c124c97bf1667c849ab9224884d2c968
charlie lee <ctlee9096@gmail.com>
2016-12-17 22:57:44 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+#require 'pry'
+def hello_t(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+        i = i + 1
+  end
 end
-
+#lib/hello.rb
+def hello_t(array)
+  #binding.pry
 # call your method here!
-
+  array.each do |name|
+  #binding.pry
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
+end
