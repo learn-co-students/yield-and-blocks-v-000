a168e80a8ae08cc6b21c82709c73595bd4964988
Scott Young <young.scottw@gmail.com>
2017-01-06 18:14:16 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -7,7 +7,23 @@ def hello_t(array)
   array
 end
 
+
+def hello_t_refactor(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
+end
+
 # call your method here!
+
+hello_t_refactored(["Tim", "Tom", "Jim"])
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
     if name.start_with?("T")
