dcb0c11fa1f17bbd5fa5cc0c57089a94ceae8560
Daniel Lempesis <danlrcn@hotmail.com>
2017-02-03 20:08:54 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
+def hello_t(array)
+  if block_given?
+    array.pop
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+    #array.shift()
 
+    return array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+hello_t(["this doesn't even matter"]) do |name|
+    if name.start_with?("T")
+        puts "Hi, #{name}"
+    end
+end
