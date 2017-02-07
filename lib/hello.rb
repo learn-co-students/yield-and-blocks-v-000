196b61b6a282dbaeba3ffc9fde05c590cffe8bd7
Pepper <axpepper@gmail.com>
2017-02-07 00:25:12 -0800

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,21 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0
+    out=[]
+    while i < array.length
+   		if array[i].upcase.start_with?("T")
+        	yield(array[i])
+        	out<<array[i]
+    	end
+    	i = i + 1
+    end
+    out
+  else
+    puts "Hey! No block was given!"
+  end
 end
-
 # call your method here!
 
+ hello_t(["Tim", "Tom", "Jim"]) do |name|
+        puts "Hi, #{name}"
+end
