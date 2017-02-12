66b6ff2b36e7a323adf74a189a00e3fce48f92d1
Dalma Boros <dalma.boros@gmail.com>
2017-02-12 18:30:34 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  t_array = []
+  array.each do |name|
+    if name.start_with?("T") || name.start_with?("t")
+      puts "Hi, #{name}"
+      t_array << name
+    end
+  end
+  return t_array
 end
 
 # call your method here!
-
