de03078348d5293c892de54a5f17b0d26b67c18a
krystlebarnes <krystlecbarnes@gmail.com>
2017-02-11 19:17:19 -0800

    add hello_t method to return names that start with T

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < array.length
+      if array[i].upcase.start_with?("T")
+        yield(array[i])
+        new_array << array[i]
+      end
+      i = i + 1
+    end
+    new_array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+        puts "Hi, #{name}"
+    end
+end
