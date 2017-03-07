be4663b29cfd1173954c581caef200039f9144f3
Anthony McCormick-Gonzales <a.j.mccormickgonzales@gmail.com>
2017-03-07 01:21:32 +0000

    Code along is broken

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0
+    t_array = []
+    while i < array.length
+      if array[i].upcase.start_with?("T")
+        yield(array[i])
+        t_array << array[i]
+      end
+      i = i + 1
+    end
+    t_array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
 
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+        puts "Hi, #{name}"
+    end
+end
