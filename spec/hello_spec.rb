a442dadbf035a8a87a95cc1c79395b0a4b42f7f3
Terry Blue <terryblue@comcast.net>
2017-03-08 00:45:25 +0000

    Done.

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,13 +1,13 @@
 require "spec_helper"
 
-describe "#hello_t" do 
+describe "#hello_t" do
   let (:names) { ["Tim", "Tom", "Jim"] }
-  it "only passes names that start with 'T' to the block" do 
+  it "only passes names that start with 'T' to the block" do
     expect{hello_t(names){|name| puts "Hi, #{name}" }}.to output("Hi, Tim\nHi, Tom\n").to_stdout
   end
 
-  it "returns an array with only names that start with 'T'" do
-    expect(hello_t(names) {|name| puts "Hi, #{name}" }).to eq(["Tim", "Tom"])
+  it "returns the original array" do
+    expect(hello_t(names) {|name| puts "Hi, #{name}" }).to eq(["Tim", "Tom", "Jim"])
   end
 
   it "is case insensitive" do
