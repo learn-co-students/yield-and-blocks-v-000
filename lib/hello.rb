61f9d94bde7ab6024c6b285facf4aa86a1914ef1
William Barela <william.barela.dev@gmail.com>
2017-03-23 05:51:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
+names = ["Tim", "Tom", "Jim"]
 
+def hello_t(array)
+  names_with_T = []
+  for name in array
+    (yield(name); names_with_T << name) if name.upcase.start_with?("T")
+  end
+  names_with_T
 end
 
 # call your method here!
-
+hello_t(names) {|name| puts "Hi, #{name}"}
