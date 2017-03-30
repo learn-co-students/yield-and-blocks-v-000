6801a3a4d04545323a9bd465914b6577732536b8
Bryan Hou <bryan.hou94@gmail.com>
2017-03-30 03:44:39 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  names_aft=[]
+  while i < array.length
+    if array[i].start_with?("T","t")
+      yield(array[i])
+      names_aft << array[i]
+    end
+    i += 1
+  end
+  names_aft
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) {|name| puts "Hi, #{name}" if name.start_with?("T","t")}
