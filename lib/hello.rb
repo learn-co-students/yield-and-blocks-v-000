b1a89d8e96c03945e304d88507c1058d46c8f554
github username <github email address>
2017-03-31 00:42:29 +0900

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
-
+def hello_t(array)
+  i = 0
+  t_arr = []
+  while i < array.length
+    if array[i].start_with?("t")||array[i].start_with?("T")
+      t_arr << array[i]
+      yield array[i]
+    end
+        i = i + 1
+  end
+  t_arr
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+        puts "Hi, #{name}"
+end
