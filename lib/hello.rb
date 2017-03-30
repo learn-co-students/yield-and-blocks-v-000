0f5892017a78ce7afd0a98ed311ceb556c0e592a
Aditi Green <asaki444@gmail.com>
2017-03-30 19:59:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
-end
-
-# call your method here!
-
+array = ["Tim", "Tom", "Jim"]
+  def hello_t(array)
+    i = 0
+    second_array = []
+     while i < array.length
+       if array[i].start_with?("T", "t") 
+         yield array[i]
+         second_array << array[i]
+       end
+       i = i + 1
+     end
+     second_array
+  end
