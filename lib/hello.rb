0f72ed9bb363ec6b9d89b630c8bc47f2c88cf784
Zain <zain@halai.us>
2017-04-07 11:06:23 -0500

    Completed lab and fixed incorrect test specs in hello_spec.rb

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
-
+def hello_t(array)
+	newArray = []
+	if block_given?
+		i = 0
+		while i < array.length
+			newArray << array[i] if yield(array[i])
+			i += 1
+		end
+	else
+		puts "No block given!"
+	end
+	newArray
 end
 
 # call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
