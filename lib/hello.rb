a57b88a257ff42ecdda79b6d0b0ba4a0a40a0d5f
Jacilyn Hayden <jacihayden@gmail.com>
2017-04-13 04:12:46 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,27 +1,3 @@
-# def hello_t
-#
-# end
-
-# call your method here!
-# Actual semi-working method from README:
-
-# def hello_t(array)
-#   i = 0
-#   while i < array.length
-#     yield(array[i])
-#     i = i + 1
-#   end
-#   array
-# end
-
-array = ["Tim", "Tom", "Jim"]
-
-# hello_t(["Tim", "Tom", "Jim"]) do |name|
-#     if name.start_with?("T")
-#         puts "Hi, #{name}"
-#     end
-# end
-
 def hello_t(array)
         names = []
         array.each do |name|
@@ -33,18 +9,5 @@ def hello_t(array)
             #     puts "Hi, #{name}"
             end
         end
-  #     }
-  # i = 0
-  # while i < array.length
-  #   yield(array[i])
-  #   i = i + 1
-  # end
-  # array
   names
 end
-
-# array do |name|
-#     if name.start_with?("T")
-#         puts "Hi, #{name}"
-#     end
-# end
