703fb3d406bb2cce0392f6f75ebfcad9ea78881e
Meredith Reece <nerdybug@gmail.com>
2017-06-05 18:34:22 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
-def hello_t
-
+def hello_t(list)
+  list.each.collect do |name|
+    if name.start_with?("T") || name.start_with?("t")
+      puts "Hi, #{name}"
+    end
+  end
+  list
 end
-
-# call your method here!
-
