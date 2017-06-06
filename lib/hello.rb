57620ec2cc87fd2e371430519a8d50bc7d4770fb
lpassamano <leighayna.josele@gmail.com>
2017-06-05 22:03:56 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(array)
   if block_given?
     i = 0
     while i < array.length
-      yield array[i]
+      yield(array[i])
       i += 1
     end
     array
@@ -18,3 +18,28 @@ hello_t(["Tim", "Tom", "Jim"]) do |name|
         puts "Hi, #{name}"
     end
 end
+
+
+#####################################################################################
+#Leigh's experiment zone!
+
+#def each_method(array)
+#  if block_given?
+#    i = 0
+#    while i < array.length
+#      yield(array[i])
+#      i += 1
+#    end
+#    array
+#  else
+#    puts "Hey! No block was given!"
+#  end
+#end
+
+#def hello_t(name)
+#  if name.start_with?("T")
+#    puts "Hi, #{name}"
+#  end
+#end
+
+#each_method(["Tammy", "Timmy", "Chaz"]) {|name| hello_t(name)}
