5d687cd8241b2ff45409c98a600c38799d28514c
Annette Maza <nettymaza@gmail.com>
2017-08-16 15:35:37 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+require 'pry'
 
+def hello_t(array)
+  i = 0
+  while i < array.length
+    i += 1
+  end
 end
 
-# call your method here!
 
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  binding.pry
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
