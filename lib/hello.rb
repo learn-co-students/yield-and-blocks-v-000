8e04300015084d65e8618acd9f6d1f3e1e7c9699
Annette Maza <nettymaza@gmail.com>
2017-08-16 15:48:13 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,32 @@
-require 'pry'
+
 
 def hello_t(array)
-  i = 0
-  while i < array.length
-    i += 1
+  if block_given?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
+
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i += 1
+#   end
+#   array
+# end
+
 
 
 # call your method here!
 hello_t(["Tim", "Tom", "Jim"]) do |name|
-  binding.pry
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
