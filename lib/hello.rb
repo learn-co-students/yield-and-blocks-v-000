ecbf6a3bfa5b5621c549c948728791f6dd77a7c4
Gustavo Villagrana <gusvilla303@gmail.com>
2017-08-25 04:27:09 +0000

    Start building the yield block

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
-def hello_t
+def hello_t(names)
+  i = 0
 
+  while i < names.size
+    yield names[i]
+    i += 1
+  end
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
