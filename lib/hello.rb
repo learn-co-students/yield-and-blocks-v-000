91f7358518c3f1d9560cac793fe809d6f8981438
Berkeley Fahrenthold <berkeley_fahrenthold@urmc.rochester.edu>
2018-01-10 21:15:10 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,17 @@
-def hello_t
+def hello_t(array)
+  i = 0
 
+if block_given?
+  while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+else
+    puts "Hey! No block was given!"
+  end
+    array
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  puts name
+end
