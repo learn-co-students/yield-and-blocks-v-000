89da1f07481b8d4b3a7309ff220cdb238e2e6a7b
smithwebtek <brad@smithwebtek.com>
2018-10-17 12:04:32 -0400

    Update hello.rb

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,42 @@
 
-# array = ["Tim", "Tom", "Jim"]
+
+# def hello_t(array) 
+#   array.each do |name|
+#     if name.start_with?("T")
+#       puts "Hi, #{name}"
+#     end
+#   end
+# end
 
 
+ 
+# # hello_t
+# (["Tim", "Tom", "Jim"]) do |name|
+#     if name.start_with?("T")
+#         puts "Hi, #{name}"
+#     end
+# end
 
-def hello_t(array) 
-  array.each do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
+
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i = i + 1
+#   end
+#   # array
+# end
+
+def hello_t(array)
+  if block_given?
+    i = 0
+    while i < array.length
+      yield(array[i])
+      i = i + 1
     end
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
+
