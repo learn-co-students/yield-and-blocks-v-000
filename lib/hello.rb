617242b9c64751b71309a2fbaf2fe387a98e560e
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-11 02:56:38 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
 
+def hello_t(names)
+  i = 0
+names.each do |name|
+if name.start_with?("T")
+    puts "Hi, #{name}"
+while i < name.length
+yield(names[i])
+    i = i + 1
 end
-
-# call your method here!
-
+end
+end
+end
